3230bf3fc0e05364612259d508c41bdd8e707103
s3nh <steam.panek@gmail.com>
2020-09-12 09:06:13 -0400

    Add onnx_convert - leukemia resnet 50 converted succesfully

--- classification/onnx_convert.py
+++ classification/onnx_convert.py
@@ -1,31 +1,43 @@
 import torch
 import torchvision 
+import typing 
+from typing import Dict
 from train import ClassificationTask
 from utils import read_config
 
-# TODO - standarize
-
 def load_model_pt(path: str) ->None:
     model = ClassificationTask()
-    return model.load_state_dict(torch.load(path))
+    model.load_state_dict(torch.load(path))
+    return model
 
-    
-def onnx_conversion(path : str) -> None:
 
-    return  torch.onnx.export(
-        #x is an input <- ffs
+def onnx_conversion(model: None, path : str, config: Dict) -> None:
+    model.eval()
+    print("Model eval succesfully")
+    input_size = config.get('input_size')
+    print(input_size)
+    x = torch.randn(config.get('batch_size'), *config.get('input_size')[::-1], requires_grad=True)
+    print(x.size)
+    torch_out = model(x)
+    print(torch_out)
+    torch.onnx.export(
+        model,
         x, 
         'leukemia_resnet50.onnx', 
         export_params=True, 
-        # TODO 
-        #input names
-        #optimization part 
+        opset_version = 10, 
+        do_constant_folding= True,
+        input_names = ['input'], 
+        output_names = ['output'], 
+        dynamic_axes={'input' : {0 : 'batch_size'}, 
+                      'output' : {0 : 'batch_size'}}
     )
-    
 
 def main():
     config = read_config()
     model=load_model_pt(path = config.get('torch_model_path')) 
-        
+    print(model)
+    onnx_conversion(model, path = '.', config = config)
+    
 if __name__ == "__main__":
     main()
